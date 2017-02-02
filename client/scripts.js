3d7f4792c500154bae9c9766c0912f41f56fd687
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-02-02 17:50:59 -0400

    2.0

--- client/scripts.js
+++ client/scripts.js
@@ -6,3 +6,14 @@ var showModal = function(imgSrc) {
 var hideModal = function() {
   $('#myModal3').modal('hide');
 }
+
+window.sr = ScrollReveal({ reset: true });
+  sr.reveal('.row1', { duration: 2000 });
+  sr.reveal('.row2', { duration: 2000 });
+  sr.reveal('.row3', { duration: 2000 });
+  sr.reveal('.row4', { duration: 2000 });
+  sr.reveal('.row5', { duration: 2000 });
+  sr.reveal('.row6', { duration: 2000 });
+  sr.reveal('.row7', { duration: 2000 });
+  sr.reveal('.row8', { duration: 2000 });
+  sr.reveal('.foot', { duration: 2000 });

97bb7588193275984f3f2a44712e4f78baa8dd5e
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-03-31 11:46:43 -0400

    added pictures

--- client/scripts.js
+++ client/scripts.js
@@ -1,18 +1,29 @@
-var showModal = function(imgSrc) {
-  $('#modalImage').attr('src', imgSrc);
-  $( '#myModal3' ).modal('show');
+var showModal = function (imgSrc) {
+  $('#modalImage').attr('src', imgSrc)
+  $('#myModal3').modal('show')
 }
 
-var hideModal = function() {
-  $('#myModal3').modal('hide');
+var hideModal = function () {
+  $('#myModal3').modal('hide')
 }
 
-window.sr = ScrollReveal({ reset: true });
-  sr.reveal('.row1', { duration: 2000 });
-  sr.reveal('.row2', { duration: 2000 });
-  sr.reveal('.row3', { duration: 2000 });
-  sr.reveal('.row4', { duration: 2000 });
-  sr.reveal('.row5', { duration: 2000 });
-  sr.reveal('.row6', { duration: 2000 });
-  sr.reveal('.row7', { duration: 2000 });
-  sr.reveal('.row8', { duration: 2000 });
+window.sr = ScrollReveal({ reset: true })
+  sr.reveal('.row1', { duration: 2000 })
+  sr.reveal('.row2', { duration: 2000 })
+  sr.reveal('.row3', { duration: 2000 })
+  sr.reveal('.row4', { duration: 2000 })
+  sr.reveal('.row5', { duration: 2000 })
+  sr.reveal('.row6', { duration: 2000 })
+  sr.reveal('.row7', { duration: 2000 })
+  sr.reveal('.row8', { duration: 2000 })
+  sr.reveal('.row9', { duration: 2000 })
+  sr.reveal('.row10', { duration: 2000 })
+  sr.reveal('.row11', { duration: 2000 })
+  sr.reveal('.row12', { duration: 2000 })
+  sr.reveal('.row13', { duration: 2000 })
+  sr.reveal('.row14', { duration: 2000 })
+  sr.reveal('.row15', { duration: 2000 })
+  sr.reveal('.row16', { duration: 2000 })
+  sr.reveal('.row17', { duration: 2000 })
+  sr.reveal('.row18', { duration: 2000 })
+  sr.reveal('.row19', { duration: 2000 })
